ba5ac409f1dce2188a4902cd919f10fe80b79a89
Khalid Sookia <khalidsookia@gmail.com>
2015-07-01 16:36:25 +0200

    Creation of errored project

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+require "vendor/test/Projet/Test.php";
---
+++ vendor/test/Projet/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Projet;
+
+class Test
+{
+    var $name = 'Marie';
+    var $age = 25;
+}
+
+
+var_dump("expression");
+
+$marie = new Test;
+
+var_dump($marie);
